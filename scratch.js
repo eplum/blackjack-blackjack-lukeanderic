0d6287ee839b4cc1ab2f5b74ec4841aee9f89214
Luke Brannigan <lukebrannigan57@gmail.com>
2019-10-22 01:44:23 -0400

    created scratch pad for practice code

---
+++ scratch.js
@@ -0,0 +1,16 @@
+const button = document.querySelectorAll(".button"); //references all html elements ".button"
+                                                     //also document.querySelector(".button") for 
+                                                     //first instance
+
+buttons.forEach(function(button) { 
+    button.addEventListener ("click", () => {
+        const paragraph = document.createElement("p"); //creates variable 'paragraph' for new <p>
+
+        paragraph.classlist.add("newParagraph");  
+
+        paragraph.textContent = "My new paragraph";
+
+        document.body.append(paragraph) //reference <body> of html doc
+    })
+});
+
