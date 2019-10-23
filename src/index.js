7284f8aa658a8b2e9ecade3630eb3a59709c80b8
Your Name <plumge@gmail.com>
2019-10-23 16:51:06 -0400

    display of dealer and user hands corrected

--- src/index.js
+++ src/index.js
@@ -9,23 +9,25 @@ const Dom = require("./utils/Dom");
 
   const dealerHand = singleDeckGame.getDealerHand();
   
-  Dom.generateCard(dealerHand.getCards()[0])
-  Dom.generateCard(dealerHand.getCards()[1]);
+  // dealerHand.foreach ()
+  // Dom.generateCard(dealerHand.getCards()[0])
+  // Dom.generateCard(dealerHand.getCards()[1]);
 
-  const table = document.querySelector(".table");
-  table.append(Dom.generateCard(dealerHand.getCards()[0]));
-  table.append(Dom.generateCard(dealerHand.getCards()[1]));
+  const dealerCards = document.querySelector(".dealer-hand");
+  dealerCards.append(Dom.generateCard(dealerHand.getCards()[0]));
+  dealerCards.append(Dom.generateCard(dealerHand.getCards()[1]));
 
 
 
   const userHand = singleDeckGame.getUserHand();
   
-  Dom.generateCard(userHand.getCards()[0])
-  Dom.generateCard(userHand.getCards()[1]);
+  const userCards = document.querySelector(".user-hand");
+  // Dom.generateCard(userHand.getCards()[0])
+  // Dom.generateCard(userHand.getCards()[1]);
 
 
-  table.append(Dom.generateCard(userHand.getCards()[0]));
-  table.append(Dom.generateCard(userHand.getCards()[1]));
+  userCards.append(Dom.generateCard(userHand.getCards()[0]));
+  userCards.append(Dom.generateCard(userHand.getCards()[1]));
 
 
 
